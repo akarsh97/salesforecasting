9a2f1d95a40a3951af003726cdccb6f962ff4dbd
akarsh97 <akarshchhabra97@gmail.com>
2018-09-26 14:05:46 +0530

    Update randomforest.py

--- randomforest.py
+++ randomforest.py
@@ -5,22 +5,23 @@ from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import xlrd
 app = Flask(__name__)
-df = pd.read_excel("Sample.xls")
-dataframe = pd.DataFrame(df)
+dataset = pd.read_excel("Sample.xls")
+df = pd.DataFrame(dataset)
 furniture = df.loc[df['Category'] == 'Furniture']
-cols = ['Row ID','Order ID','Ship Date','Ship Mode','Customer ID','Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
-furniture.drop(cols,axis=1,inplace=True)#sort with dates
-furniture = furniture.sort_values('Order Date')
-dt = furniture['Order Date'].values.astype(int)
-sales = furniture['Sales'].values
-dt = dt.reshape(-1, 1)
-dt_train,dt_test,sales_train,sales_test = train_test_split(dt,sales,test_size = 0.2)
-regressor = RandomForestRegressor(n_estimators=300)
-regressor = regressor.fit(dt_train,sales_train)
-sales_pred = regressor.predict(dt_test)
-@app.route("/randomforest")
-def randomforest():
-   	data = {'SalesPredictions' : sales_pred.tolist()}
+furniture.drop(df.columns.difference(['Order Date','Sales']), 1, inplace=True)
+
+@app.route("/rf")
+def rf():
+	dt = furniture['Order Date'].values.astype('int')
+	sales = furniture['Sales'].values
+	dt = dt.reshape(-1, 1)
+	dt_train,dt_test,sales_train,sales_test = train_test_split(dt,sales,test_size = 0.2)
+	regressor = RandomForestRegressor(n_estimators=300)
+	regressor = regressor.fit(dt_train,sales_train)
+	sales_pred = regressor.predict(dt_test)
+   	data = {
+   	'SalesPredictions' : list(sales_pred)
+   	}
    	return jsonify(data)
 if __name__ == '__main__':
 	app.run(debug=True)
